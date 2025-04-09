73428f19760ac16c015d9b17a8ef537588660c6a
_uixer_ <evgeniu.ermolenko@gmail.com>
2025-04-10 01:29:27 +0200

    Added seeding for Components

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt");
 const uuid = require("uuid");
 const { types, manufacturers } = require("./seederConstants");
 
+const { getRandomInt } = require("../utils/getRandomInt");
+
 async function main() {
 	const hashPassword = bcrypt.hashSync("pass", 7);
 	const activationLink = uuid.v4();
@@ -31,8 +33,6 @@ async function main() {
 		});
 	});
 
-	//const typesFromDB = await prisma.type.findMany();
-
 	//---------- manufacturers
 	manufacturers.forEach(async (manufacturerItem) => {
 		await prisma.manufacturer.upsert({
@@ -42,6 +42,50 @@ async function main() {
 				name: manufacturerItem,
 			},
 		});
+	});
+
+	const typesFromDB = await prisma.type.findMany();
+	const manufacturersFromDB = await prisma.manufacturer.findMany();
+
+	await prisma.component.deleteMany();
+
+	typesFromDB.forEach(async (type) => {
+		for (let i = 1; i <= 2; i++) {
+			await prisma.component.upsert({
+				where: {
+					name: `${type.name}-${
+						manufacturersFromDB[
+							getRandomInt(0, manufacturersFromDB.length - 1)
+						].name
+					}`,
+				},
+				update: {},
+				create: {
+					name: `${type.name}-${
+						manufacturersFromDB[
+							getRandomInt(0, manufacturersFromDB.length - 1)
+						].name
+					} ${getRandomInt(0, 10000)}`,
+					model: `Model: ${type.name}-${
+						manufacturersFromDB[
+							getRandomInt(0, manufacturersFromDB.length - 1)
+						].name
+					} ${getRandomInt(0, 10000)}`,
+					price:
+						100 * getRandomInt(0, manufacturersFromDB.length - 1),
+					image: "img",
+					level: getRandomInt(1, 7),
+					electricConsumption: Math.random(),
+					watt: Math.random(),
+					typeId: typesFromDB[getRandomInt(0, typesFromDB.length - 1)]
+						.id,
+					manufacturerId:
+						manufacturersFromDB[
+							getRandomInt(0, manufacturersFromDB.length - 1)
+						].id,
+				},
+			});
+		}
 	});
 }
 
